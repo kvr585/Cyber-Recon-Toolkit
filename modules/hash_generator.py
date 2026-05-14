d297f46aca94c09d1a6821a15d0345f9b6cf36fd
Veera Bhadhra <kkvr0676@gmail.com>
2026-05-13 23:32:41 -0400

    Added working hash generator module

--- modules/hash_generator.py
+++ modules/hash_generator.py
@@ -1,2 +1,15 @@
-def hash_menu():
-    print("HASH GENERATOR")
+import hashlib
+
+def  hash_menu():
+	print("\n===== HASH GENERATOR =====")
+
+	text = input("Enter text:")
+
+	md5_hash = hashlib.md5(text.encode()).hexdigest()
+	sha1_hash = hashlib.sha1(text.encode()).hexdigest()
+	sha256_hash = hashlib.sha256(text.encode()).hexdigest()
+
+
+	print("\nMD5:", md5_hash)
+	print("SHA1:",sha1_hash)
+	print("SHA256:", sha256_hash)
